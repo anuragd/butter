ced23bebbafc584494db56d839bdea6aaa898336
Anurag Dutta <anudutta@cisco.com>
2018-07-16 11:08:08 -0700

    Fixed valid/invalid behavior of TextArea and Input on User interaction

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <components.Input label="Input" invalid="This is an error message"/>
+        <components.Input label="Input" invalid="This is an error message" value="Error"/>
         <components.Button label="Button"/>
         <components.TextArea label="TextArea" invalid="This is an error message"/>
       </div>
