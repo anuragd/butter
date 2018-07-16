120a80c3f237463814ba2941d1d86d00cafdf905
Anurag Dutta <anudutta@cisco.com>
2018-07-16 16:00:13 -0700

    Basic drop down. Accepts list of options, label and a change handler function.

--- example/src/App.js
+++ example/src/App.js
@@ -6,10 +6,10 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <components.Input label="Input" invalid="This is an error message" value="Error"/>
+      	<components.Dropdown label="Select" options={[{id:0,value:"Option 1"},{id:1,value:"Option 2"},{id:2,value:"Option 3"},{id:3,value:"Option 4"}]}/>
+        <components.Input label="Input"/>
         <components.Button label="Button"/>
-        <components.TextArea label="TextArea" invalid="This is an error message"/>
-        <components.Dropdown label="Select"/>
+        <components.TextArea label="TextArea"/>
       </div>
     )
   }
--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -23,43 +23,66 @@ export default class Dropdown extends Component {
     /**
      * Description of prop "clickHandler".
      */
-    clickHandler: PropTypes.func,
+    changeHandler: PropTypes.func,
     /**
      * Description of prop "disabled".
      */
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
+    /**
+     * Description of prop "options".
+     */
+    options: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      value: PropTypes.string
+    })).isRequired
   }
 
   constructor(props) {
     super(props)
     this.state = {
+      open: false,
+      value:null
     };
     this.clickHandler = this.clickHandler.bind(this)
+    this.optionClickHandler = this.optionClickHandler.bind(this)
   }
 
   clickHandler(e) {
-    if(this.props.clickHandler) this.props.clickHandler()
+    this.setState({open: !this.state.open})
+  }
+  
+  optionClickHandler(id,value) {
+    this.setState({value:value, open:false})
+    if(this.props.changeHandler instanceof Function) this.props.changeHandler({id:id, value: value})
   }
 
   render() {
     const {
       label,
+      options,
       disabled
     } = this.props
 
+    const optionsList = options.map((option) =>
+      <li key={option.id} onClick={() => this.optionClickHandler(option.id, option.value)}>
+        <div className={styles.list_bg}></div>
+        <div className={styles.option_label}>{option.value}</div>
+      </li>
+    )
+
     return (
       <div className={styles.dropdown_container}>
-        <div className={styles.header}>
-          <div className={styles.float_label}>{label}</div>
-          <img className={styles.arrow} src={svg} />
+        <div className={this.state.value?styles.focus_label:styles.float_label}>{label}</div>
+        <div className={this.state.open?styles.open_list:styles.list}>
+          <div className={this.state.open?styles.open_header:styles.header} onClick={this.clickHandler}>
+            {this.state.value?'':label}
+            <div className={styles.selected_option}>{this.state.value}</div>
+            <img className={styles.arrow} src={svg} />
+          </div>
+          <ul className={styles.options}>
+            {optionsList}
+          </ul>
         </div>
-        <ul className={styles.list}>
-          <li>Option 1</li>
-          <li>Option 2</li>
-          <li>Option 3</li>
-          <li>Option 4</li>
-          <li>Option 5</li>
-        </ul>
       </div>
     )
   }
