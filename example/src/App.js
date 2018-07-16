6ea01993abac0e68cfbfa12901be9400e8e479d5
Anurag Dutta <anudutta@cisco.com>
2018-07-16 16:44:28 -0700

    Added control disable, default option and disabled option capabilities

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,14 @@ export default class App extends Component {
   render () {
     return (
       <div>
-      	<components.Dropdown label="Select" options={[{id:0,value:"Option 1"},{id:1,value:"Option 2"},{id:2,value:"Option 3"},{id:3,value:"Option 4"}]}/>
+      	<components.Dropdown 
+      		label="Select" 
+      		options={[
+      			{id:0,value:"Option 1"},
+      			{id:1,value:"Option 2", disabled: true},
+      			{id:2,value:"Option 3"},
+      			{id:3,value:"Option 4"}]}
+      		disabled/>
         <components.Input label="Input"/>
         <components.Button label="Button"/>
         <components.TextArea label="TextArea"/>
