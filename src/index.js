f938b2dda0a0bcb52486bbd9e1e4f502d50ac7b7
Anurag Dutta <anudutta@cisco.com>
2018-07-17 16:31:04 -0700

    Merge commit errors fix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import {default as Input} from './components/Input/Input'
 import {default as Button} from './components/Button/Button'
 import {default as TextArea} from './components/TextArea/TextArea'
-<<<<<<< HEAD
-=======
 import {default as Dropdown} from './components/Dropdown/Dropdown'
-<<<<<<< HEAD
->>>>>>> 82b830b6ab8cc4380544fecd7196338a46c242b9
-=======
 import {default as Radio} from './components/Radio/Radio'
 import {default as ProgressBar} from './components/ProgressBar/ProgressBar'
->>>>>>> 7c235999d3bdd97df1ebabbf00814c8a030a6b36
 
 let components = {
 	Input,
 	Button,
-<<<<<<< HEAD
-	TextArea
-=======
 	TextArea,
-<<<<<<< HEAD
-	Dropdown
->>>>>>> 82b830b6ab8cc4380544fecd7196338a46c242b9
-=======
 	Dropdown,
 	Radio,
 	ProgressBar
->>>>>>> 7c235999d3bdd97df1ebabbf00814c8a030a6b36
 }
 
 export { components }
