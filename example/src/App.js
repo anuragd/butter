a09f14d1b7472abf8ca6580fd968c13ed665330f
Anurag Dutta <anudutta@cisco.com>
2018-07-16 17:56:13 -0700

    Basic Radio Button functionality (animation is a WIP)

--- example/src/App.js
+++ example/src/App.js
@@ -6,6 +6,12 @@ export default class App extends Component {
   render () {
     return (
       <div>
+      	<components.Radio 
+      		options={[
+      			{id:0,value:"Option 1"},
+      			{id:1,value:"Option 2", disabled: true},
+      			{id:2,value:"Option 3"},
+      			{id:3,value:"Option 4"}]}/>
       	<components.Dropdown 
       		label="Select" 
       		options={[
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import {default as Input} from './components/Input/Input'
 import {default as Button} from './components/Button/Button'
 import {default as TextArea} from './components/TextArea/TextArea'
 import {default as Dropdown} from './components/Dropdown/Dropdown'
+import {default as Radio} from './components/Radio/Radio'
 
 let components = {
 	Input,
 	Button,
 	TextArea,
-	Dropdown
+	Dropdown,
+	Radio
 }
 
 export { components }
